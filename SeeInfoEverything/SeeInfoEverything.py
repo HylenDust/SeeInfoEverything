9ff2843410813f0249c27e1980c7b62193e367c9
HylenDust <84090115+HylenDust@users.noreply.github.com>
2021-05-30 15:32:02 -0700

    Update

---
+++ SeeInfoEverything/SeeInfoEverything.py
@@ -0,0 +1,19 @@
+import time
+import platform
+print("Welcome to SeeInfoEverything by Project Naitik.")
+print("Loading Info Of Your Machine...")
+time.sleep(3)
+print(platform.uname())
+print("Machine Code: ")
+print(platform.machine())
+print("Machine Code: ")
+print(platform.node())
+print('34')
+print(platform.python_build())
+a="43"
+print(a)
+print(platform.python_compiler())
+print(a)
+print(platform.java_ver(release='', vendor='', vminfo=('', '', ''), osinfo=('', '', '')))
+print(a)
+print(platform.win32_ver(release='', version='', csd='', ptype=''))
